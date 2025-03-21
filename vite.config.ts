b51f09c6c02d085216d84effb5868f8df6696956
Goose_tG <jarredhampton@hotmail.com>
2025-03-20 22:41:39 -0400

    added allowedHosts in viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
     port: 3000,
   },
   preview: {
-    host: '0.0.0.0',
-    port: 3000,
+      allowedHosts: ['candidate-search-app-f8ny.onrender.com'], // Add this host to allowed hosts
   },
 });
